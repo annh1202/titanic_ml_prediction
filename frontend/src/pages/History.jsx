090ec7061fc9d59a3a599be225e96392cc3de011
MaiThiKimThoa <2451012102thoa@ou.edu.vn>
2026-06-27 17:49:39 +0700

    Update History.jsx

--- frontend/src/pages/History.jsx
+++ frontend/src/pages/History.jsx
@@ -70,11 +70,11 @@
             margin:5px;
         }
 
-        .btn1{
+        .btn-new{
             background:#003366;
         }
 
-        .btn2{
+        .btn-delete{
             background:red;
         }
 
@@ -133,8 +133,8 @@
     </table>
 
     <div class="button">
-        <button class="btn1">Dự đoán mới</button>
-        <button class="btn2">Xóa lịch sử</button>
+        <button class="btn-new">Dự đoán mới</button>
+        <button class="btn-delete">Xóa lịch sử</button>
     </div>
 
 </div>
